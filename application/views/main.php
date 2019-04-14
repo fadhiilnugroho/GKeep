63c60cca762e90ba4e07c729fce4e9a11c5cbe6b
fadhiilnugroho <47170965+fadhiilnugroho@users.noreply.github.com>
2019-04-14 17:59:02 +0700

    Add files via upload

---
+++ application/views/main.php
@@ -0,0 +1,223 @@
+
+<html lang="en">
+
+<head>
+
+
+
+  <title>GOOGLE KEEP</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Custom styles for this template -->
+  <link href="css/simple-sidebar.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+ 
+
+    <!-- Page Content -->
+    <div id="page-content-wrapper">
+
+      <nav style="background-color: white" class="navbar navbar-expand border-bottom">
+        <button class="btn" id="menu-toggle"><img src="https://img.icons8.com/material/24/000000/menu.png"></button>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+              <a style="width: 200px" class="nav-link" href="#">flashdatas</a>
+          <form class="form-inline my-6 my-lg-0">
+         <input style="width: 700px;background-color:lightgrey" class="form-control mr-sm-6" type="search" placeholder="Telusuri" aria-label="Search">
+            </form>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                logo setting
+              </a>
+              <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item" href="#">Setelan</a>
+                <a class="dropdown-item" href="#">Kirim masukan</a>
+                <a class="dropdown-item" href="https://support.google.com/keep/#topic=6262468">Bantuan</a>
+                <a class="dropdown-item" href="#">Aplikasi yang didownload</a>
+                <a class="dropdown-item" href="#">Pintasan keyboard</a>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" href="#">Something else here</a>
+              </div>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+               <img src="https://img.icons8.com/material/24/000000/circled-menu.png">
+              </a>
+              <div style="font-size: 11px;font-family: roboto" class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                  <div class="card border-light " style="width: 320px; height: 480px; padding: 28px">
+                    <div class="row"  style="margin-bottom: 10px">
+                        <div class="col-sm-4">
+                            <div class="container-fluid">
+                            <img src="https://img.icons8.com/material/64/000000/user.png">
+                            <div class="text-centers" style="width: 84px;height: 20px">Akun Google</div>
+                            </div>
+                        </div>
+                        <div class="col-sm-4">
+                            <div class="container-fluid">
+                            <img src="https://img.icons8.com/color/64/000000/google-logo.png">
+                            <div class="text-centers" style="width: 84px;height: 20px">Telusuri</div>
+                            </div>
+                        </div>
+                        <div class="col-sm-4">
+                            <div class="container-fluid">
+                            <img src="https://img.icons8.com/color/64/000000/google-maps.png">
+                            <div class="text-centers" style="width: 84px">Maps</div>
+                            </div>
+                        </div>
+                      </div>
+                    <div class="row"  style="margin-bottom: 10px">
+                        <div class="col-sm-4">
+                            <div class="container-fluid">
+                            <img src="https://img.icons8.com/color/64/000000/youtube-play.png">
+                            <div class="text-centers" style="width: 84px">Youtube</div>
+                            </div>
+                        </div>
+                        <div class="col-sm-4">
+                            <div class="container-fluid">
+                            <img src="https://img.icons8.com/color/64/000000/google-news.png">
+                            <div class="text-centers" style="width: 84px">Berita</div>
+                            </div>
+                        </div>
+                        <div class="col-sm-4">
+                            <div class="container-fluid">
+                            <img src="https://img.icons8.com/color/64/000000/gmail-login.png">
+                            <div class="text-centers" style="width: 84px">Gmail</div>
+                            </div>
+                        </div>
+                      </div>
+                    <div class="row" style="margin-bottom: 10px">
+                        <div class="col-sm-4">
+                            <div class="container-fluid">
+                            <img style="width: 64px; height: 64px" src="http://www.xanjero.com/wp-content/uploads/2018/11/new-Google-Contacts-desktop-web-app.png">
+                            <div class="text-centers" style="width: 84px">Kontak</div>
+                            </div>
+                        </div>
+                        <div class="col-sm-4">
+                            <div class="container-fluid">
+                            <img src="https://img.icons8.com/color/64/000000/google-drive.png">
+                            <div class="text-centers" style="width: 84px">Drive</div>
+                            </div>
+                        </div>
+                        <div class="col-sm-4">
+                            <div class="container-fluid">
+                            <img src="https://img.icons8.com/color/64/000000/google-calendar.png">
+                            <div class="text-centers" style="width: 84px">Kalender</div>
+                            </div>
+                        </div>
+                      </div>
+                       <div class="row"  style="margin-bottom: 10px">
+                        <div class="col-sm-4">
+                            <div class="container-fluid">
+                            <img src="https://img.icons8.com/color/64/000000/google-translate.png">
+                            <div class="text-centers" style="width: 84px">Terjemahan</div>
+                            </div>
+                        </div>
+                        <div class="col-sm-4">
+                            <div class="container-fluid">
+                            <img style="width: 64px;height:  64px" src="https://www.gstatic.com/images/branding/product/2x/photos_96dp.png">
+                            <div class="text-centers" style="width: 84px">Foto</div>
+                            </div>
+                        </div>
+                        <div class="col-sm-4">
+                            <div class="container-fluid">
+                            <div class="text-centers" style="width: 84px">Belanja</div>
+                            </div>
+                        </div>
+                      </div>
+                    
+                  </div>
+              </div>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                logo profil
+              </a>
+              <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item" href="#">akun</a>
+                <a class="dropdown-item" href="#">
+                <div class="btn btn-light"> keluar</div>
+                  </a>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </nav>
+         <div class="d-flex" id="wrapper">
+
+    <!-- Sidebar -->
+    <div style="background-color: white; width: 428px" id="sidebar-wrapper">
+      <div class="list-group">
+        <div class="row " style="padding: 16px">
+        <svg class="col-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="gb_jc"><path d="M9 21c0 .55.45 1 1 1h4c.55 0 1-.45 1-1v-1H9v1zm3-19C8.14 2 5 5.14 5 9c0 2.38 1.19 4.47 3 5.74V17c0 .55.45 1 1 1h6c.55 0 1-.45 1-1v-2.26c1.81-1.27 3-3.36 3-5.74 0-3.86-3.14-7-7-7zm2.85 11.1l-.85.6V16h-4v-2.3l-.85-.6A4.997 4.997 0 0 1 7 9c0-2.76 2.24-5 5-5s5 2.24 5 5c0 1.63-.8 3.16-2.15 4.1z"></path></svg>
+        <div class="col-8 text-rigth" href="#">Catatan</div>
+        </div>  
+          
+        <div class="row " style="padding: 16px">
+        <svg class="col-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="gb_jc"><path d="M18 17v-6c0-3.07-1.63-5.64-4.5-6.32V4c0-.83-.67-1.5-1.5-1.5s-1.5.67-1.5 1.5v.68C7.64 5.36 6 7.92 6 11v6H4v2h16v-2h-2zm-2 0H8v-6c0-2.48 1.51-4.5 4-4.5s4 2.02 4 4.5v6zm-4 5c1.1 0 2-.9 2-2h-4c0 1.1.9 2 2 2z"></path></svg>
+        <div class="col" href="#">Pengingat</div>
+        </div>  
+        <div class="dropdown-divider"></div>
+        <div class="row text-muted" style="margin-left: 24px"> Label </div>
+        <div class="row " style="padding: 18px">
+        <svg class="col-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="gb_jc"><path d="M20.41 4.94l-1.35-1.35c-.78-.78-2.05-.78-2.83 0L13.4 6.41 3 16.82V21h4.18l10.46-10.46 2.77-2.77c.79-.78.79-2.05 0-2.83zm-14 14.12L5 19v-1.36l9.82-9.82 1.41 1.41-9.82 9.83z"></path></svg>
+        <div class="col" href="#"> Edit Label</div>
+        </div>  
+        <div class="dropdown-divider"></div>
+        <div class="row " style="padding: 16px">
+        <svg class="col-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="gb_jc"><path d="M20.54 5.23l-1.39-1.68C18.88 3.21 18.47 3 18 3H6c-.47 0-.88.21-1.16.55L3.46 5.23C3.17 5.57 3 6.02 3 6.5V19c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V6.5c0-.48-.17-.93-.46-1.27zM6.24 5h11.52l.83 1H5.42l.82-1zM5 19V8h14v11H5zm11-5.5l-4 4-4-4 1.41-1.41L11 13.67V10h2v3.67l1.59-1.59L16 13.5z"></path></svg>
+        <div class="col" href="#" >Arsipkan</div>
+        </div>  
+          
+        <div class="row " style="padding: 16px">
+        <svg class="col-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="gb_jc"><path d="M15 4V3H9v1H4v2h1v13c0 1.1.9 2 2 2h10c1.1 0 2-.9 2-2V6h1V4h-5zm2 15H7V6h10v13z"></path><path d="M9 8h2v9H9zm4 0h2v9h-2z"></path></svg>
+        <div class="col" href="#" >Sampah</div>
+        </div>
+          
+      </div>
+    </div>
+    <!-- /#sidebar-wrapper -->
+             
+
+      <div class="container-fluid" style="height: 80px; margin-top: 50px">
+          <div class="border rounded" style="margin: auto; width: 562px; height: 42px" onclick=""> 
+            <div class="row" style="visibility: hidden">
+                <input type="text" placeholder="Judul">
+            </div>
+            <div class="row" style="border: 2px; border-color: black;">
+                <input style="font-size: 18px;" type="text" class="text-muted col-9" placeholder="Buat catatan...">
+                <img class="col-1" src="https://img.icons8.com/small/24/000000/checked-checkbox.png">
+                <img src="https://img.icons8.com/material-rounded/24/000000/illustrator.png">
+                <img class="col-1" src="https://img.icons8.com/material-outlined/24/000000/picture.png">
+              </div>
+          </div>
+      </div>
+    </div>
+    <!-- /#page-content-wrapper -->
+
+  </div>
+  <!-- /#wrapper -->
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Menu Toggle Script -->
+  <script>
+    $("#menu-toggle").click(function(e) {
+      e.preventDefault();
+      $("#wrapper").toggleClass("toggled");
+    });
+  </script>
+
+</body>
+
+</html>
